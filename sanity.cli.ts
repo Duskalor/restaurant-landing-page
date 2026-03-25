9a9e26db9ca1ba6c8419c9a4cbb9be536f729b2e
duskalor <duskalor1@gmail.com>
2026-03-25 01:41:13 -0500

    fix: use env vars in sanity.cli.ts, remove hardcoded appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,8 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: 'q2ywqdj2',
-    dataset: 'production',
+    projectId: process.env.PUBLIC_SANITY_PROJECT_ID ?? '',
+    dataset: process.env.PUBLIC_SANITY_DATASET ?? 'production',
   },
   deployment: {
     appId: 'gt159rhskafe5cv0n8zgk3ek',
